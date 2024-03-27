23947a4c63a1b0fdecb91023bc6d85a7a9d2442f
Daffa <57443780+ds-raw@users.noreply.github.com>
2024-03-27 23:28:58 +0700

    Update Prediction.py

--- pages/Prediction.py
+++ pages/Prediction.py
@@ -26,15 +26,77 @@ register_page(
 )
 ####################### DATASET #############################
 # Load the model from the file
-loaded_model = joblib.load("Hasil RFR.joblib")
+loaded_model = joblib.load("aws.joblib")
 
 ####################### PAGE LAYOUT #############################
 # Updated layout with dropdown options based on one-hot encoding
 layout = html.Div([
     html.H1("Random Forest Regressor Prediction App"),
     dcc.Input(id='lt-input', type='number', placeholder='Enter LT (m2)'),
-    dcc.Input(id='distance-input', type='number', placeholder='Enter distance_ke_pusatkota'),
-    dcc.Input(id='lebar-jalan-input', type='number', placeholder='Enter Lebar Jalan Depan (m)'),
+    dcc.Dropdown(id='air-label-dropdown',
+                options=[
+                    {'label': 'Tidak Ada', 'value': 0},
+                    {'label': 'Ada', 'value': 1},
+                ],
+                placeholder='Select Ketersediaan Air'
+                ),
+    dcc.Dropdown(id='listrik-label-dropdown',
+                options=[
+                    {'label': 'Tidak Ada', 'value': 0},
+                    {'label': 'Ada', 'value': 1},
+                ],
+                placeholder='Select Ketersediaan Listrik'
+                ),
+    dcc.Dropdown(id='Distance-Category-Label-dropdown',
+                options=[
+                    {'label': 'Dekat', 'value': 3},
+                    {'label': 'Sedang', 'value': 2},
+                    {'label': 'Jauh', 'value': 1},
+                    {'label': 'Sangat Jauh', 'value': 0},
+                ],   
+                placeholder='Select Kategori Jarak Pusat Kota'
+                ),
+  #  dcc.Input(id='distance-input', type='number', placeholder='Enter distance_ke_pusatkota'),
+   dcc.Input(id='lebar-jalan-input', type='number', placeholder='Enter Lebar Jalan Depan (m)'),
+   dcc.Dropdown(
+       id='clusters-dropdown',
+       options=[
+           {'label': f'Cluster_{i}', 'value': i} for i in range(3)
+       ],
+       placeholder='Select Clusters'
+   ),
+   dcc.Dropdown(
+       id='peruntukan-dropdown',
+       options=[
+           {'label': 'Fasilitas Umum', 'value': 'Peruntukan_Fasilitas Umum'},
+           {'label': 'Kawasan Industri', 'value': 'Peruntukan_Kawasan Industri'},
+           {'label': 'Kawasan Perdagangan dan Jasa', 'value': 'Peruntukan_Kawasan Perdagangan dan Jasa'},
+           {'label': 'Kawasan Peruntukan Perkebunan', 'value': 'Peruntukan_Kawasan Peruntukan Perkebunan'},
+           {'label': 'Pemukiman Perkotaan', 'value': 'Peruntukan_Pemukiman Perkotaan'},
+       
+       ],
+       placeholder='Select Peruntukan'
+   ),
+   dcc.Dropdown(
+       id='hap-dropdown',
+       options=[
+           {'label': 'Hak Atas Properti_AJB', 'value': 'Hak Atas Properti_AJB'},
+           {'label': 'Hak Atas Properti_Girik', 'value': 'Hak Atas Properti_Girik'},
+         #  {'label': 'Hak Atas Properti_Gross Akte', 'value': 'Hak Atas Properti_Gross Akte'},
+           {'label': 'Hak Atas Properti_HGB diatas HPL', 'value': 'Hak Atas Properti_HGB diatas HPL'},
+           {'label': 'Hak Atas Properti_HP', 'value': 'Hak Atas Properti_HP'},
+           {'label': 'Hak Atas Properti_PPJB', 'value': 'Hak Atas Properti_PPJB'},
+           
+           {'label': 'Hak Atas Properti_SHGB', 'value': 'Hak Atas Properti_SHGB'},
+         #  {'label': 'Hak Atas Properti_SHGU', 'value': 'Hak Atas Properti_SHGU'},
+           {'label': 'Hak Atas Properti_SHM', 'value': 'Hak Atas Properti_SHM'},
+           
+           {'label': 'Hak Atas Properti_SHMSRS ', 'value': 'Hak Atas Properti_SHMSRS '},
+           {'label': 'Hak Atas Properti_SIPPT', 'value': 'Hak Atas Properti_SIPPT'},
+        #   {'label': 'Hak Atas Properti_SPH', 'value': 'Hak Atas Properti_SPH'},
+       ],
+       placeholder='Select Hak Atas Properti'
+   ),
     dcc.Dropdown(
         id='kota-kabupaten-dropdown',
         options=[
@@ -68,45 +130,8 @@ layout = html.Div([
         ],
         placeholder='Select Kota/Kabupaten'
     ),
-    dcc.Dropdown(
-        id='clusters-dropdown',
-        options=[
-            {'label': f'Cluster {i}', 'value': i} for i in range(3)
-        ],
-        placeholder='Select Clusters'
-    ),
-    dcc.Dropdown(
-        id='peruntukan-dropdown',
-        options=[
-            {'label': 'Fasilitas Umum', 'value': 'Peruntukan_Fasilitas Umum'},
-            {'label': 'Kawasan Industri', 'value': 'Peruntukan_Kawasan Industri'},
-            {'label': 'Kawasan Perdagangan dan Jasa', 'value': 'Peruntukan_Kawasan Perdagangan dan Jasa'},
-            {'label': 'Kawasan Peruntukan Perkebunan', 'value': 'Peruntukan_Kawasan Peruntukan Perkebunan'},
-            {'label': 'Pemukiman Perkotaan', 'value': 'Peruntukan_Pemukiman Perkotaan'},
-            {'label': 'Perikanan (Tambak, Kolam, Perairan Umum)', 'value': 'Peruntukan_Perikanan (Tambak, Kolam, Perairan Umum)'},
-        ],
-        placeholder='Select Peruntukan'
-    ),
-    dcc.Dropdown(
-        id='hap-dropdown',
-        options=[
-            {'label': 'Hak Atas Properti_AJB', 'value': 'Hak Atas Properti_AJB'},
-            {'label': 'Hak Atas Properti_Girik', 'value': 'Hak Atas Properti_Girik'},
-            {'label': 'Hak Atas Properti_Gross Akte', 'value': 'Hak Atas Properti_Gross Akte'},
-            {'label': 'Hak Atas Properti_HGB diatas HPL', 'value': 'Hak Atas Properti_HGB diatas HPL'},
-            {'label': 'Hak Atas Properti_HP', 'value': 'Hak Atas Properti_HP'},
-            {'label': 'Hak Atas Properti_PPJB', 'value': 'Hak Atas Properti_PPJB'},
-            
-            {'label': 'Hak Atas Properti_SHGB', 'value': 'Hak Atas Properti_SHGB'},
-            {'label': 'Hak Atas Properti_SHGU', 'value': 'Hak Atas Properti_SHGU'},
-            {'label': 'Hak Atas Properti_SHM', 'value': 'Hak Atas Properti_SHM'},
-            
-            {'label': 'Hak Atas Properti_SHMSRS ', 'value': 'Hak Atas Properti_SHMSRS '},
-            {'label': 'Hak Atas Properti_SIPPT', 'value': 'Hak Atas Properti_SIPPT'},
-            {'label': 'Hak Atas Properti_SPH', 'value': 'Hak Atas Properti_SPH'},
-        ],
-        placeholder='Select Hak Atas Properti'
-    ),
+    
+    
     dcc.Dropdown(
         id='bentuk-tapak-dropdown',
         options=[
@@ -120,16 +145,16 @@ layout = html.Div([
         ],
         placeholder='Select Bentuk Tapak'
     ),
-    dcc.Dropdown(
-        id='kualitas-wilayah-dropdown',
-        options=[
-            {'label': 'Bawah', 'value': 'Kualitas Wilayah Sekitar_Bawah'},
-            {'label': 'Kumuh', 'value': 'Kualitas Wilayah Sekitar_Kumuh'},
-            {'label': 'Menengah', 'value': 'Kualitas Wilayah Sekitar_Menengah'},
-            {'label': 'Mewah', 'value': 'Kualitas Wilayah Sekitar_Mewah'},
-        ],
-        placeholder='Select Kualitas Wilayah Sekitar'
-    ),
+   # dcc.Dropdown(
+    #    id='kualitas-wilayah-dropdown',
+     #   options=[
+      #      {'label': 'Bawah', 'value': 'Kualitas Wilayah Sekitar_Bawah'},
+       #     {'label': 'Kumuh', 'value': 'Kualitas Wilayah Sekitar_Kumuh'},
+        #    {'label': 'Menengah', 'value': 'Kualitas Wilayah Sekitar_Menengah'},
+         #   {'label': 'Mewah', 'value': 'Kualitas Wilayah Sekitar_Mewah'},
+        #],
+        #placeholder='Select Kualitas Wilayah Sekitar'
+    #),
     dcc.Dropdown(
         id='kondisi-wilayah-dropdown',
         options=[
@@ -137,7 +162,7 @@ layout = html.Div([
             {'label': 'Hijau', 'value': 'Kondisi Wilayah Sekitar_Hijau'},
             {'label': 'Industri', 'value': 'Kondisi Wilayah Sekitar_Industri'},
             {'label': 'Komersial', 'value': 'Kondisi Wilayah Sekitar_Komersial'},
-            {'label': 'Pemerintahan', 'value': 'Kondisi Wilayah Sekitar_Pemerintahan'},
+        #    {'label': 'Pemerintahan', 'value': 'Kondisi Wilayah Sekitar_Pemerintahan'},
             {'label': 'Perumahan', 'value': 'Kondisi Wilayah Sekitar_Perumahan'},
         ],
         placeholder='Select Kondisi Wilayah Sekitar'
@@ -162,39 +187,46 @@ layout = html.Div([
 # Updated callback function
 @callback(
     Output('output-predictions', 'children'),
-    Input('predict-button', 'n_clicks'),
-    State('lt-input', 'value'),
-    State('distance-input', 'value'),
-    State('lebar-jalan-input', 'value'),
-    State('clusters-dropdown', 'value'),
-    State('peruntukan-dropdown', 'value'),
-    State('hap-dropdown', 'value'),  # Include Hak Atas Properti value
-    State('kota-kabupaten-dropdown', 'value') , # Include Kondisi Tapak value
-    State('bentuk-tapak-dropdown', 'value'),  # Include Bentuk Tapak value
-    State('kualitas-wilayah-dropdown', 'value'),  # Include Kualitas Wilayah Sekitar value
-    State('kondisi-wilayah-dropdown', 'value'),  # Include Kondisi Wilayah Sekitar value
-    State('kondisi-tapak-dropdown', 'value') , # Include Kondisi Tapak value
-    
+    [Input('predict-button', 'n_clicks')],
+    [State('lt-input', 'value'),
+     State('air-label-dropdown', 'value'),
+     State('listrik-label-dropdown', 'value'),
+     State('Distance-Category-Label-dropdown', 'value'),
+     State('lebar-jalan-input', 'value'),
+     State('clusters-dropdown', 'value'),
+     State('peruntukan-dropdown', 'value'),
+     State('hap-dropdown', 'value'),
+     State('kota-kabupaten-dropdown', 'value'),
+     State('bentuk-tapak-dropdown', 'value'),
+     State('kondisi-wilayah-dropdown', 'value'),
+     State('kondisi-tapak-dropdown', 'value')]
+
 )
-def update_output_predictions(n_clicks, lt_input, distance_input, lebar_jalan_input, clusters_dropdown, peruntukan_dropdown, hap_dropdown, bentuk_tapak_dropdown, kualitas_wilayah_dropdown, kondisi_wilayah_dropdown, 
-                              kondisi_tapak_dropdown,kota_kabupaten_dropdown):
-    if n_clicks == 0 or None in [lt_input, distance_input, lebar_jalan_input, clusters_dropdown, peruntukan_dropdown, hap_dropdown, kota_kabupaten_dropdown,bentuk_tapak_dropdown, kualitas_wilayah_dropdown, kondisi_wilayah_dropdown, 
-                                  kondisi_tapak_dropdown]:
+def update_prediction(n_clicks, lt_input, air_label_input, listrik_label_input, distance_category_input, lebar_jalan_input, clusters, peruntukan, hap, kota_kabupaten, bentuk_tapak, kondisi_wilayah, kondisi_tapak):
+    if n_clicks == 0 or None in [lt_input, air_label_input, listrik_label_input, distance_category_input, lebar_jalan_input, clusters, peruntukan, hap, kota_kabupaten, bentuk_tapak, kondisi_wilayah, kondisi_tapak]:
         return None
+
+    # Convert air_label boolean to integer (0 for False, 1 for True)
+   
+
+    # Convert distance_category to integer (3 to 0)
+
+
 
     user_input = [
         float(lt_input),
-        float(distance_input),
+         int(air_label_input),
+        int(listrik_label_input),
+        int(distance_category_input),
         float(lebar_jalan_input),
-        clusters_dropdown,
-        peruntukan_dropdown,
-        hap_dropdown,
-        kota_kabupaten_dropdown,
-        bentuk_tapak_dropdown,
-        kualitas_wilayah_dropdown,
-        kondisi_wilayah_dropdown,
-        kondisi_tapak_dropdown,
-        
+        clusters,
+        peruntukan,
+        hap,
+        kota_kabupaten,
+        bentuk_tapak,
+       # kualitas_wilayah,  
+        kondisi_wilayah,
+        kondisi_tapak
     ]
 
     prediction = make_predictions(loaded_model, user_input)
@@ -207,42 +239,81 @@ def update_output_predictions(n_clicks, lt_input, distance_input, lebar_jalan_in
 
 
 
-# Function to make predictions using the loaded model
 def make_predictions(model, new_data):
-    lt, distance, lebar_jalan, cluster, peruntukan, hap, bentuk_tapak, kualitas_wilayah, kondisi_wilayah, kondisi_tapak,kota_kabupaten_dropdown = new_data
-    
-# Create a DataFrame with the input features
-    input_df = pd.DataFrame([[float(lt), float(distance), float(lebar_jalan)] +  [0]*70], 
-                        columns=['LT (m2)', 'distance_ke_pusatkota', 'Lebar Jalan Depan (m)', 'Clusters_0', 'Clusters_1', 'Clusters_2', 
-                                 'Peruntukan_Fasilitas Umum', 'Peruntukan_Kawasan Industri', 'Peruntukan_Kawasan Perdagangan dan Jasa', 'Peruntukan_Kawasan Peruntukan Perkebunan', 
-                                 'Peruntukan_Pemukiman Perkotaan', 'Peruntukan_Perikanan (Tambak, Kolam, Perairan Umum)', 'Hak Atas Properti_AJB', 'Hak Atas Properti_Girik',
-                                 'Hak Atas Properti_Gross Akte', 'Hak Atas Properti_HGB diatas HPL', 'Hak Atas Properti_HP', 'Hak Atas Properti_PPJB', 'Hak Atas Properti_SHGB', 
-                                 'Hak Atas Properti_SHGU', 'Hak Atas Properti_SHM', 'Hak Atas Properti_SHMSRS ', 'Hak Atas Properti_SIPPT', 'Hak Atas Properti_SPH', 'Kota/Kabupaten_Kabupaten Bandung', 
-                                 'Kota/Kabupaten_Kabupaten Bandung Barat', 'Kota/Kabupaten_Kabupaten Bekasi', 'Kota/Kabupaten_Kabupaten Bogor', 'Kota/Kabupaten_Kabupaten Ciamis', 
-                                 'Kota/Kabupaten_Kabupaten Cianjur', 'Kota/Kabupaten_Kabupaten Cirebon', 'Kota/Kabupaten_Kabupaten Garut', 'Kota/Kabupaten_Kabupaten Indramayu', 
-                                 'Kota/Kabupaten_Kabupaten Karawang', 'Kota/Kabupaten_Kabupaten Kuningan', 'Kota/Kabupaten_Kabupaten Majalengka', 'Kota/Kabupaten_Kabupaten Pangandaran', 
-                                 'Kota/Kabupaten_Kabupaten Purwakarta', 'Kota/Kabupaten_Kabupaten Subang', 'Kota/Kabupaten_Kabupaten Sukabumi', 'Kota/Kabupaten_Kabupaten Sumedang', 
-                                 'Kota/Kabupaten_Kabupaten Tasikmalaya', 'Kota/Kabupaten_Kota Bandung', 'Kota/Kabupaten_Kota Banjar', 'Kota/Kabupaten_Kota Bekasi', 'Kota/Kabupaten_Kota Bogor', 
-                                 'Kota/Kabupaten_Kota Cimahi', 'Kota/Kabupaten_Kota Cirebon', 'Kota/Kabupaten_Kota Depok', 'Kota/Kabupaten_Kota Sukabumi', 'Kota/Kabupaten_Kota Tasikmalaya', 
-                                 'Bentuk Tapak_Kipas', 'Bentuk Tapak_Letter L', 'Bentuk Tapak_Ngantong', 'Bentuk Tapak_Persegi', 'Bentuk Tapak_Persegi Panjang', 'Bentuk Tapak_Tidak Beraturan', 
-                                 'Bentuk Tapak_Trapesium', 'Kualitas Wilayah Sekitar_Bawah', 'Kualitas Wilayah Sekitar_Kumuh', 'Kualitas Wilayah Sekitar_Menengah', 'Kualitas Wilayah Sekitar_Mewah', 
-                                 'Kondisi Wilayah Sekitar_Campuran', 'Kondisi Wilayah Sekitar_Hijau', 'Kondisi Wilayah Sekitar_Industri', 'Kondisi Wilayah Sekitar_Komersial', 
-                                 'Kondisi Wilayah Sekitar_Pemerintahan', 'Kondisi Wilayah Sekitar_Perumahan', 'Kondisi Tapak_Darat / Kering', 'Kondisi Tapak_Tanah Mentah', 
-                                 'Kondisi Tapak_Tanah Rawa', 'Kondisi Tapak_Tanah Siap Dikembangkan (Tanah Matang)', 'Kondisi Tapak_Tanah dalam Pengembangan'])
-    
-    # Set the corresponding features to 1
-    input_df[f'Clusters_{cluster}'] = 1
-    input_df[peruntukan] = 1
-    input_df[hap] = 1
-    input_df[kota_kabupaten_dropdown] = 1
-    input_df[bentuk_tapak] = 1
-    input_df[kualitas_wilayah] = 1
-    input_df[kondisi_wilayah] = 1
-    input_df[kondisi_tapak] = 1
-    
-    
-    prediction = model.predict(input_df)
-    
-    formatted_prediction = "{:,.2f}".format(prediction[0])
-    
-    return formatted_prediction
+    # Define all feature names used during training
+    all_features = ['LT (m2)', 'Air_Label', 'Listrik_Label', 'Distance_Category_Label', 'Lebar Jalan Depan (m)']
+    all_features += [f'Clusters_{i}' for i in range(3)]
+    all_features += ['Peruntukan_Fasilitas Umum', 'Peruntukan_Kawasan Industri',
+                     'Peruntukan_Kawasan Perdagangan dan Jasa', 'Peruntukan_Kawasan Peruntukan Perkebunan',
+                     'Peruntukan_Pemukiman Perkotaan']
+    all_features += ['Hak Atas Properti_AJB', 'Hak Atas Properti_Girik', 'Hak Atas Properti_HGB diatas HPL',
+                     'Hak Atas Properti_HP', 'Hak Atas Properti_PPJB', 'Hak Atas Properti_SHGB',
+                     'Hak Atas Properti_SHM', 'Hak Atas Properti_SHMSRS ', 'Hak Atas Properti_SIPPT']
+    all_features += ['Kota/Kabupaten_Kabupaten Bandung',
+       'Kota/Kabupaten_Kabupaten Bandung Barat',
+       'Kota/Kabupaten_Kabupaten Bekasi',
+        'Kota/Kabupaten_Kabupaten Bogor',
+       'Kota/Kabupaten_Kabupaten Ciamis', 
+    'Kota/Kabupaten_Kabupaten Cianjur',
+       'Kota/Kabupaten_Kabupaten Cirebon', 
+        'Kota/Kabupaten_Kabupaten Garut',
+       'Kota/Kabupaten_Kabupaten Indramayu',
+       'Kota/Kabupaten_Kabupaten Karawang',
+       'Kota/Kabupaten_Kabupaten Kuningan',
+       'Kota/Kabupaten_Kabupaten Majalengka',
+       'Kota/Kabupaten_Kabupaten Pangandaran',
+       'Kota/Kabupaten_Kabupaten Purwakarta',
+       'Kota/Kabupaten_Kabupaten Subang', 
+        'Kota/Kabupaten_Kabupaten Sukabumi',
+       'Kota/Kabupaten_Kabupaten Sumedang',
+       'Kota/Kabupaten_Kabupaten Tasikmalaya', 
+        'Kota/Kabupaten_Kota Bandung',
+       'Kota/Kabupaten_Kota Banjar', 
+        'Kota/Kabupaten_Kota Bekasi',
+       'Kota/Kabupaten_Kota Bogor',
+        'Kota/Kabupaten_Kota Cimahi',
+       'Kota/Kabupaten_Kota Cirebon',
+        'Kota/Kabupaten_Kota Depok',
+       'Kota/Kabupaten_Kota Sukabumi', 
+        'Kota/Kabupaten_Kota Tasikmalaya']
+    all_features += [ 'Bentuk Tapak_Kipas', 
+        'Bentuk Tapak_Letter L', 
+        'Bentuk Tapak_Ngantong',
+       'Bentuk Tapak_Persegi', 
+        'Bentuk Tapak_Persegi Panjang',
+       'Bentuk Tapak_Tidak Beraturan', 
+            'Bentuk Tapak_Trapesium']
+    all_features += ['Kondisi Wilayah Sekitar_Campuran', 'Kondisi Wilayah Sekitar_Hijau',
+                     'Kondisi Wilayah Sekitar_Industri', 'Kondisi Wilayah Sekitar_Komersial',
+                     'Kondisi Wilayah Sekitar_Perumahan']
+    all_features += ['Kondisi Tapak_Darat / Kering', 'Kondisi Tapak_Tanah Mentah', 'Kondisi Tapak_Tanah Rawa',
+                     'Kondisi Tapak_Tanah Siap Dikembangkan (Tanah Matang)',
+                     'Kondisi Tapak_Tanah dalam Pengembangan']
+
+    # Initialize input data list with zeros for all features
+    input_data = [0] * len(all_features)
+
+    # Update input data with user's selections
+    lt, air_label, listrik_label, distance_category, lebar_jalan, cluster, peruntukan, hap, kota_kabupaten, bentuk_tapak, kondisi_wilayah, kondisi_tapak = new_data
+    input_data[all_features.index('LT (m2)')] = lt
+    input_data[all_features.index('Air_Label')] = air_label
+    input_data[all_features.index('Listrik_Label')] = listrik_label
+    input_data[all_features.index('Distance_Category_Label')] = distance_category
+    input_data[all_features.index('Lebar Jalan Depan (m)')] = lebar_jalan
+    input_data[all_features.index(f'Clusters_{cluster}')] = 1
+    input_data[all_features.index(peruntukan)] = 1
+    input_data[all_features.index(hap)] = 1
+    input_data[all_features.index(kota_kabupaten)] = 1
+    input_data[all_features.index(bentuk_tapak)] = 1
+    input_data[all_features.index(kondisi_wilayah)] = 1
+    input_data[all_features.index(kondisi_tapak)] = 1
+
+    # Make predictions using the loaded model
+    prediction = model.predict([input_data])
+
+    return prediction[0]
+
+
+
+
+

98f519c49242350b05782bd0b571ea1be17888c5
Daffa <57443780+ds-raw@users.noreply.github.com>
2024-05-28 21:57:32 +0700

    update ver 2

--- pages/Geospatial.py
+++ pages/Geospatial.py
@@ -4,11 +4,10 @@ Created on Tue Feb  6 23:20:03 2024
 
 @author: daffa
 """
-from dash import html, register_page, dash_table
-import pandas as pd 
-from dash import dcc, html, callback
+from dash import html, register_page, dcc, callback
+import pandas as pd
 import plotly.express as px
-from dash.dependencies import Input, Output #, callback # If you need callbacks, import it here.
+from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
 register_page(
@@ -19,10 +18,27 @@ register_page(
 )
 
 # DATASET
-df = pd.read_excel("data aws.xlsx")
+try:
+    df = pd.read_excel("New Model 3_Clusters.xlsx")
+except Exception as e:
+    df = pd.DataFrame()  # Fallback to an empty dataframe if loading fails
+    print(f"Error loading dataset: {e}")
+
+# Create a single 'Clusters' column if not exists
+if 'Clusters' not in df.columns:
+    # Assuming Clusters_0, Clusters_1, and Clusters_2 are mutually exclusive, create 'Clusters' column
+    df['Clusters'] = df[['Clusters_0', 'Clusters_1', 'Clusters_2']].idxmax(axis=1)
 
 # MAPBOX CHART FUNCTION
 def create_map_chart(df):
+    if df.empty:
+        return px.scatter_mapbox(
+            title='No data available',
+            mapbox_style="carto-positron",
+            zoom=10,
+            center={"lat": 0, "lon": 0}
+        )
+    
     figure = px.scatter_mapbox(
         df, 
         lat='latitude', 
@@ -32,8 +48,18 @@ def create_map_chart(df):
         mapbox_style="carto-positron",
         zoom=10,
         center={"lat": df['latitude'].mean(), "lon": df['longitude'].mean()},
+        hover_data={
+            'Peruntukan': True,
+            'Harga Tanah (m2)': True,
+            'LT (m2)': True,
+            'Kondisi Wilayah Sekitar': True,
+            'Kondisi Tapak': True,
+            'Clusters': True,  # Optionally hide Clusters if not needed in hover
+            'latitude': True,  # Optionally hide latitude if not needed in hover
+            'longitude': True  # Optionally hide longitude if not needed in hover
+        }
     )
-    figure.update_layout(height=720, width=1280)
+    figure.update_layout(height=580, width=1280)
     return figure
 
 # FILTER OPTIONS
@@ -52,7 +78,7 @@ listrik_dropdown = dcc.Dropdown(id='ada-listrik-dropdown', options=listrik_optio
 
 # PAGE LAYOUT
 layout = html.Div(children=[
-    html.H3('Geospatial Analysis'),
+    html.H3('Geospatial Analysis' ,style={'text-align': 'center'}),
     dbc.Row([
         dbc.Col(peruntukan_dropdown, width=3),
         dbc.Col(kondisi_wilayah_dropdown, width=3),

f5af330ddd0cfaa1c1bc2523ca95832efba7e18c
Daffa <57443780+ds-raw@users.noreply.github.com>
2024-05-28 21:56:14 +0700

    update ver 2

--- navbar.py
+++ navbar.py
@@ -54,8 +54,8 @@ def create_navbar():
                 children=[  # Add as many menu items as you need
                     dbc.DropdownMenuItem("Home", href='/'),
                     dbc.DropdownMenuItem(divider=True),
-                    dbc.DropdownMenuItem("Dataset", href='/Dataset'),
-                    dbc.DropdownMenuItem("Page 3", href='/page3'),
+                    dbc.DropdownMenuItem("Prediction 4 Cluster", href='/Prediction4'),
+                    dbc.DropdownMenuItem("Geospatial 4 Cluster", href='/Geospatial4'),
                 ],
             ),
             
